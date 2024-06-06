15efe8982c817f7cc8d669ca91dc19ea3d572283
TastyKayKs <KevinKay1990@Gmail.com>
2024-06-05 21:47:35 -0500

    Update FFT.ps1
    bitwise maffs r faster

--- FFT.ps1
+++ FFT.ps1
@@ -3,15 +3,14 @@ Function FFT($Arr){
 
     If($Len -le 1){Return $Arr}
 
-    $Len_Over_2 = [Math]::Floor(($Len/2))
-
+    $Len_Over_2 = $Len -shr 1
     $Output  = New-Object System.Numerics.Complex[] $Len
 
     $EvenArr = @()
     $OddArr  = @()
 
     For($i = 0; $i -lt $Len; $i++){
-        If($i % 2){
+        If($i -band 1){
             $OddArr+=$Arr[$i]
         }Else{
             $EvenArr+=$Arr[$i]
